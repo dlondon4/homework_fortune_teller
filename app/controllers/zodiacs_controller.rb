b6b55d22975ba76df7323458562db70e0ac0ac98
dlondon4 <dlondon4@gmail.com>
2014-10-30 12:20:10 -0500

    Creatures work

--- app/controllers/zodiacs_controller.rb
+++ app/controllers/zodiacs_controller.rb
@@ -79,7 +79,9 @@ class ZodiacsController < ApplicationController
 
   def creature
     # @prediction = "Business and financial success makes you happy and satisfied, Pisces. You also look forward to moving on. The downside of this flush period is that people who aren't particularly trustworthy might decide to latch onto your coattails for their own purposes. Some might even ask for loans. Be careful about the ones you assist. They might not be honest with you. Don't fall for any sob stories."
-
+    @z = Zodiac.find_by({ :creature => params[:the_creature] })
+    @fortune = @z.fortune
+    @sign = @z.sign
     render 'creature'
   end
 end
